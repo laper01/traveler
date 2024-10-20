f62dfb1637abc537ee521b539c91f38b93ee5c84
laper01 <yusufyahya0107@gmail.com>
2024-10-20 18:12:44 +0800

    add-tailwindcss-and-profile

--- app/Profile.tsx
+++ app/Profile.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Profile = () => {
   return (
-    <View>
+    <View  >
       <Text>Profile</Text>
     </View>
   )
@@ -11,4 +11,4 @@ const Profile = () => {
 
 export default Profile
 
-const styles = StyleSheet.create({})
+// const styles = StyleSheet.create({})
